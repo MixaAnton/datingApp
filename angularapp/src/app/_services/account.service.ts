800c34b4161271cbcbe4208155a881eb5998c6fe
antonicm <mihajlo.antonic@lanaco.com>
2023-08-15 09:55:37 +0200

    routing

--- angularapp/src/app/_services/account.service.ts
+++ angularapp/src/app/_services/account.service.ts
@@ -9,8 +9,10 @@ import { User } from '../_models/user';
 export class AccountService {
 
   baseUrl = 'https://localhost:5001/api/';
+
   private currentUserSource = new ReplaySubject<User>();
   currentUser$ = this.currentUserSource.asObservable();
+
   constructor(private http: HttpClient) { }
 
   login(model: any) {
